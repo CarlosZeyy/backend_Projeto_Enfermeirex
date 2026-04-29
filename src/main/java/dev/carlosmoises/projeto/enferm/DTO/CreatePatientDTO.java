5fa04bd3178228fb355353fe894549bc8a975bdf
CarlosZeyy <carloszeeyy@gmail.com>
2026-04-28 21:35:31 -0300

    fix: add obs field in patient dto

--- src/main/java/dev/carlosmoises/projeto/enferm/DTO/CreatePatientDTO.java
+++ src/main/java/dev/carlosmoises/projeto/enferm/DTO/CreatePatientDTO.java
@@ -4,5 +4,5 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record CreatePatientDTO(@NotBlank String name, @NotBlank @Size(min = 10, max = 15) String phone,
-                               String address) {
+                               String address, String obs) {
 }
