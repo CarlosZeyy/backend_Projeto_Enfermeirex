a048e0c1ba3176077c62f101ec994806248dcc1e
CarlosZeyy <carloszeeyy@gmail.com>
2026-04-28 21:28:05 -0300

    fix: createPatientDTO fields

--- src/main/java/dev/carlosmoises/projeto/enferm/DTO/CreatePatientDTO.java
+++ src/main/java/dev/carlosmoises/projeto/enferm/DTO/CreatePatientDTO.java
@@ -3,6 +3,6 @@ package dev.carlosmoises.projeto.enferm.DTO;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record CreatePatientDTO(@NotBlank String name, @NotBlank @Size(min = 10, max = 15) String phone, String address,
-                               String obs) {
+public record CreatePatientDTO(@NotBlank String name, @NotBlank @Size(min = 10, max = 15) String phone,
+                               String address) {
 }
